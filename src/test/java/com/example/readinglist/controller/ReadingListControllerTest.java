4b440eadf6e3039d34af59dca7d33609f462bf7d
Tinu <kcyangyi@gmail.com>
2018-10-31 09:47:24 +0800

    commit

--- src/test/java/com/example/readinglist/controller/ReadingListControllerTest.java
+++ src/test/java/com/example/readinglist/controller/ReadingListControllerTest.java
@@ -1,9 +1,13 @@
 package com.example.readinglist.controller;
 
-import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class ReadingListControllerTest {
 
     @Test
